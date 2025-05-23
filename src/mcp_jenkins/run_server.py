0920e32013cf89c32b454e14fd02e2c3ea1fdd45
Andrei M <andrei@ideabulbs.com>
2025-05-23 06:17:54 +0200

    file/directory restructure

--- src/mcp_jenkins/run_server.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-def main():
-    script_path = os.path.join(os.path.dirname(__file__), 'docker', 'run')
-    subprocess.run(['/bin/sh', script_path])
-
-if __name__ == '__main__':
-    main()
